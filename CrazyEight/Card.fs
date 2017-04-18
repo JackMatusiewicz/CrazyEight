98acea6e71b52cfe7ff0d9a17035847b10c918d4
Jack Matusiewicz <jack.matusiewicz@outlook.com>
2017-04-18 21:26:00 +0100

    Initial Commit

---
+++ CrazyEight/Card.fs
@@ -0,0 +1,56 @@
+﻿module Card
+    type Rank = 
+        | Ace | Two | Three | Four | Five | Six | Seven
+        | Eight | Nine | Ten | Jack | Queen | King
+
+    type Suit = Diamond | Heart | Spade | Club
+
+    let ranks = [Ace; Two; Three; Four; Five; Six; Seven; Eight; Nine; Ten; Jack; Queen; King]
+    let suits = [Diamond; Heart; Spade; Club]
+
+    type Card = {Face : Rank; Suit : Suit}
+
+    type Deck = Deck of Card list
+    type Hand = Hand of Card list
+
+    type Player = {Name : string; Hand : Hand}
+
+    let createDeck () : Deck =
+        let rec createDeckForSuit (suit : Suit) (ranks : Rank list) (acc : Card list) : Card list =
+            match ranks with
+            | [] -> acc
+            | hd :: tl ->
+                createDeckForSuit suit tl ({Face=hd; Suit = suit}::acc)
+
+        let rec createDeckForSuits (suits : Suit list) (acc : Card list) : Card list =
+            match suits with
+            | [] -> acc
+            | hd :: tl -> createDeckForSuits tl ((createDeckForSuit hd ranks []) @ acc)
+        Deck (createDeckForSuits suits [])
+
+    let createMegaDeck (numberOfDecks : int) : Deck =
+        let rec createDecks (numberOfDecks : int) (acc : Card list) : Deck =
+            match numberOfDecks with
+            | 0 -> Deck acc
+            | _ ->
+                let newDeck = createDeck ()
+                match newDeck with
+                | Deck d -> createDecks (numberOfDecks-1) (d @ acc)
+        createDecks numberOfDecks []
+
+    let shuffle (deck : Deck) : Deck =
+        let random = System.Random()
+        match deck with
+        | Deck d ->
+            Deck (d |> List.sortBy (fun value -> random.Next()))
+
+    let deal (deck : Deck) : (Card * Deck) option =
+        match deck with
+        | Deck d ->
+            match d with
+            | [] -> None
+            | hd::tl -> (hd, Deck tl) |> Some
+
+    let numberOfCardsInDeck (deck : Deck) : int =
+        match deck with
+        | Deck d -> d |> List.length
---
+++ Test/Card.fs
@@ -0,0 +1,20 @@
+﻿module CardTest
+
+open NUnit.Framework
+
+[<Test>]
+let ``Given a deck when I deal once then deck reduces in size by one`` () =
+    let deck = Card.createDeck ()
+    let sizeOfOriginalDeck = deck |> Card.numberOfCardsInDeck
+    let result = Card.deal deck
+    match result with
+    | None -> Assert.Fail("There should be output")
+    | Some (card, newDeck) ->
+        let sizeOfNewDeck = newDeck |> Card.numberOfCardsInDeck
+        Assert.That(sizeOfNewDeck, Is.EqualTo(sizeOfOriginalDeck - 1))
+
+[<Test>]
+let ``Given an empty deck when I deal I get nothing back`` () =
+    let deck = Card.Deck []
+    let result = Card.deal deck
+    Assert.That(result, Is.EqualTo(None))
